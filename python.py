2931800f51dcc21df5482e893e561789554f3135
Refal <reitshels@gmail.com>
2026-06-18 16:30:00 +0000

    feat: add name greeter

---
+++ python.py
@@ -0,0 +1,10 @@
+#لو المدحل كان هالاسم المحدد, الطباعة بيكون الجملة
+name = input("enter your name: ")
+if (name == "refal"): #hehe
+    print("hi " + name + ", welcome to my space!")
+if (name == "ziad"): #hubby
+    print("hi " + name + ", welcome to my space!")
+if (name == "abdullah"): #hb
+    print("hi " + name + ", welcome to my space!")
+
+    
